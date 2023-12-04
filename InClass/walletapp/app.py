768f6359da2b1878bce10a134d3f8048ffc2dd07
Kumporn Pengtong <archuron.gaming@gmail.com>
2023-12-04 16:26:18 +0700

    Inclass Dec4 2023

---
+++ InClass/walletapp/app.py
@@ -0,0 +1,15 @@
+def test01():
+    a = 5
+    # print(s)
+
+def test02():
+    c1 = 3
+    c2 = 3
+    code = str(c1) + str(c2)
+    print(code)
+
+def test03():
+    s1 = '10'
+    s2 = '21'
+    sum = float(s1) + float(s2)
+    print(sum)
